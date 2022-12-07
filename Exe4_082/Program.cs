f9cdeb4675ef19bd4c9eadf4cb969944fd65e3ec
Chendy Lundy <chendy.lundy.ft21@mail.umy.ac.id>
2022-12-07 15:46:09 +0700

    add method class Node and declaration variable

---
+++ Exe4_082/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe4_082
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int info;
+            public Node next;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
