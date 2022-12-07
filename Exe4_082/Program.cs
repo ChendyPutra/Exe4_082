abe2fbfa68ef50baf6e167076d7fd094f1d933e1
Chendy Lundy <chendy.lundy.ft21@mail.umy.ac.id>
2022-12-07 15:53:00 +0700

    add looping with method public node

--- Exe4_082/Program.cs
+++ Exe4_082/Program.cs
@@ -12,6 +12,11 @@ namespace Exe4_082
         {
             public int info;
             public Node next;
+            public Node(int i, Node n)
+            {
+                info = i;
+                next = n;
+            }
         }
         static void Main(string[] args)
         {
